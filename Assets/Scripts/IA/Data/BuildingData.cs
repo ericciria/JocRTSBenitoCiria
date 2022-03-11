76e0d30bf227601eae3eead3e6f778fda0d29a39
ericciria <ericciria_6624@hotmail.com>
2022-03-11 12:36:26 +0100

    Preparat scriptable objects i fix UI scale

---
+++ Assets/Scripts/IA/Data/BuildingData.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New BuildingData", menuName = "Building Data", order = 52)]
+public class BuildingData : MonoBehaviour
+{
+    [SerializeField] private string buildingName;
+    [SerializeField] private string description;
+    [SerializeField] private Sprite icon;
+    [SerializeField] private int moneyCost;
+    [SerializeField] private int metalCost;
+    [SerializeField] private int energyCost;
+    [SerializeField] private int attackDamage; //nomes cuan es una torreta
+    [SerializeField] private int maxHealth;
+
+    public string BuildingName
+    {
+        get
+        {
+            return buildingName;
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return description;
+        }
+    }
+
+    public Sprite Icon
+    {
+        get
+        {
+            return icon;
+        }
+    }
+
+    public int MoneyCost
+    {
+        get
+        {
+            return moneyCost;
+        }
+    }
+    public int MetalCost
+    {
+        get
+        {
+            return metalCost;
+        }
+    }
+    public int EnergyCost
+    {
+        get
+        {
+            return energyCost;
+        }
+    }
+
+    public int AttackDamage
+    {
+        get
+        {
+            return attackDamage;
+        }
+    }
+
+    public int MaxHealth
+    {
+        get
+        {
+            return maxHealth;
+        }
+    }
+}
---
+++ Assets/Scripts/IA/Data/UnitData.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New UnitData", menuName = "Unit Data", order = 51)]
+public class UnitData : ScriptableObject
+{
+    [SerializeField] private string unitName;
+    [SerializeField] private string description;
+    [SerializeField] private Sprite icon;
+    [SerializeField] private int moneyCost;
+    [SerializeField] private int metalCost;
+    [SerializeField] private int attackDamage;
+    [SerializeField] private string typeOfUnit; 
+    [SerializeField] private string damageMultiplierType; // string que indica el tipus d'unitat contra el que es mes fort
+    [SerializeField] private int damageMultiplierAmount; // string que indica el tipus d'unitat contra el que es mes fort
+    [SerializeField] private int maxHealth;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float attackDistance;
+    [SerializeField] private float attackCooldown;
+
+    public string UnitName
+    {
+        get
+        {
+            return unitName;
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return description;
+        }
+    }
+
+    public Sprite Icon
+    {
+        get
+        {
+            return icon;
+        }
+    }
+
+    public int MoneyCost
+    {
+        get
+        {
+            return moneyCost;
+        }
+    }
+    public int MetalCost
+    {
+        get
+        {
+            return metalCost;
+        }
+    }
+
+    public int AttackDamage
+    {
+        get
+        {
+            return attackDamage;
+        }
+    }
+    public string TypeOfUnit
+    {
+        get
+        {
+            return typeOfUnit;
+        }
+    }
+    public string DamageMultiplierType
+    {
+        get
+        {
+            return damageMultiplierType;
+        }
+    }
+    public int DamageMultiplierAmount
+    {
+        get
+        {
+            return damageMultiplierAmount;
+        }
+    }
+    public int MaxHealth
+    {
+        get
+        {
+            return maxHealth;
+        }
+    }
+    public float MovementSpeed
+    {
+        get
+        {
+            return movementSpeed;
+        }
+    }
+    public float AttackDistance
+    {
+        get
+        {
+            return attackDistance;
+        }
+    }
+    public float AttackCooldown
+    {
+        get
+        {
+            return attackCooldown;
+        }
+    }
+}
